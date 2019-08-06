8dae72a77abfc38d2c75b30bbe1e9180280d9ee1
Your Name <you@example.com>
2019-08-06 14:26:01 +0800

    持久化

--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -1,16 +1,37 @@
 package com.community.controller;
 
+import com.community.mapper.UserMapper;
+import com.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    @Autowired
+    private UserMapper userMapper;
 
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+            return "index";
+
+
+    }
 }
