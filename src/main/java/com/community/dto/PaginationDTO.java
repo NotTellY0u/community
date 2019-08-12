3c11a71646a75bbcb7161169d5d43a041afdc8af
Your Name <yangfulin123@qq.com>
2019-08-12 16:29:55 +0800

    完成个人页面发布的问题功能

--- src/main/java/com/community/dto/PaginationDTO.java
+++ src/main/java/com/community/dto/PaginationDTO.java
@@ -17,21 +17,10 @@ public class PaginationDTO {
     private Integer totalPage;
 
 
-    public void setPagination(Integer totalCount, Integer page, Integer size) {
-
-        if (totalCount % size == 0) {
-            totalPage = totalCount / size;
-        } else {
-            totalPage = totalCount / size + 1;
-        }
-
-        if (page < 1) {
-            page = 1;
-        }
-        if (page > totalPage) {
-            page = totalPage;
-        }
+    public void setPagination(Integer totalPage, Integer page) {
+        this.totalPage = totalPage;
         this.page = page;
+
         pages.add(page);
         for (int i = 1; i <= 3; i++) {
             if (page - i > 0) {
--- src/main/java/com/community/mapper/QuestionMapper.java
+++ src/main/java/com/community/mapper/QuestionMapper.java
@@ -20,5 +20,8 @@ public interface QuestionMapper {
     Integer count();
 
     @Select("select * from question where creator = #{userId} limit #{offset},#{size}")
-    List<Question> list(@Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+    List<Question> listByUserId(@Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(@Param("userId") Integer userId);
 }
