fe6685d6984605f88da9165ffbd8bf8e8df41f3c
Your Name <yangfulin123@qq.com>
2019-08-13 14:52:06 +0800

    去掉多余引用

--- src/main/java/com/community/interceptor/WebConfig.java
+++ src/main/java/com/community/interceptor/WebConfig.java
@@ -3,11 +3,8 @@ package com.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
