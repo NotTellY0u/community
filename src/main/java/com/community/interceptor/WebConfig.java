be9471528e0f8465f24b19e3546e032f5263930c
Your Name <yangfulin123@qq.com>
2019-08-13 16:21:53 +0800

    修正样式被拦截

--- src/main/java/com/community/interceptor/WebConfig.java
+++ src/main/java/com/community/interceptor/WebConfig.java
@@ -3,10 +3,12 @@ package com.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
