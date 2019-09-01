e15cccaea12ff7c652076998028e4371ea1ae217
yfl <yangfulin123@qq.com>
2019-09-01 22:39:08 +0800

    格式调整

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -2,18 +2,18 @@ function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
     $.ajax({
-        type:"POST",
-        url:url,
-        data:{
-         "parentId":questionId,
-            "content":content,
-            "type":1
+        type: "POST",
+        url: url,
+        data: {
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         },
-        success:function (response) {
+        success: function (response) {
             console.log(response);
-            
+
         },
-        dataType:"json"
+        dataType: "json"
     });
     console.log(questionId);
     console.log(content);
