d56d71709b78f4c2e2578bb7677706228d8affad
yfl <yangfulin123@qq.com>
2019-09-01 22:38:35 +0800

    回复

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -1,4 +1,20 @@
-    function post(){
-        var questionId = $("#question_id").val();
-        console.log(questionId);
-    }
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type:"POST",
+        url:url,
+        data:{
+         "parentId":questionId,
+            "content":content,
+            "type":1
+        },
+        success:function (response) {
+            console.log(response);
+            
+        },
+        dataType:"json"
+    });
+    console.log(questionId);
+    console.log(content);
+}
