9490489d97c056e8f634874ebd1bb110acc94ebe
yfl <yangfulin123@qq.com>
2019-09-02 22:50:00 +0800

    回复页面

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -3,15 +3,27 @@ function post() {
     var content = $("#comment_content").val();
     $.ajax({
         type: "POST",
-        url: url,
-        data: {
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
             "parentId": questionId,
             "content": content,
             "type": 1
-        },
+        }),
         success: function (response) {
-            console.log(response);
-
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=e9762d4cc8816d8d0cf7&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                }else {
+                    alert(response.message);
+                }
+            }
         },
         dataType: "json"
     });
