5ad23c0de544feeaf41f314cd02da69e4805fb1d
Your Name <yangfulin123@qq.com>
2019-10-21 09:30:10 +0800

    question“”

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -1,12 +1,14 @@
-
 /*
 *提交回复
 */
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
+}
 
-    if(!content){
+function comment2target(targetId, type, content) {
+    if (!content) {
         alert("不能回复空内容")
         return;
     }
@@ -15,9 +17,9 @@ function post() {
         url: "/comment",
         contentType: 'application/json',
         data: JSON.stringify({
-            "parentId": questionId,
+            "parentId": targetId,
             "content": content,
-            "type": 1
+            "type": type
         }),
         success: function (response) {
             if (response.code == 200) {
@@ -25,11 +27,11 @@ function post() {
             } else {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
-                    if(isAccepted){
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=e9762d4cc8816d8d0cf7&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
-                        window.localStorage.setItem("closable",true);
+                        window.localStorage.setItem("closable", true);
                     }
-                }else {
+                } else {
                     alert(response.message);
                 }
             }
@@ -37,25 +39,46 @@ function post() {
         dataType: "json"
     });
 }
+
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-" + commentId).val();
+    comment2target(commentId, 2, content);
+}
+
 /*
 * 展开二级评论
 * */
 function collapseComments(e) {
     var id = e.getAttribute("data-id");
-    var comments = $("#comment-"+id);
+    var comments = $("#comment-" + id);
 
     //获取一下二级评论的展开状态
     var collapse = e.getAttribute("data-collapse");
-    if(collapse){
+    if (collapse) {
         //折叠二级评论
         comments.removeClass("in");
         e.removeAttribute("data-collapse");
         e.classList.remove("active");
-    }else {
+    } else {
+        var subCommentContainer = $("#comment-" + id);
+
+        if(subCommentContainer.children().length)
+
+        $.getJSON("/comment/"+id,function (data) {
+            $.each(data.data,function (index,comment) {
+               var c = $("<div/>",{
+                    "class":"col-lg-12 col-md-12 col-sm-12 col-xs-12 comments",
+                    html:comment.content
+                });
+                subCommentContainer.prepend(c);
+            });
+
         //展开二级评论
         comments.addClass("in");
         //标记二级评论展开状态
-        e.setAttribute("data-collapse","in");
+        e.setAttribute("data-collapse", "in");
         e.classList.add("active");
+        });
     }
 }
