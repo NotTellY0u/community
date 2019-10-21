d18dee3a37f8172d1946cf7fb30b8c7336a1bdc4
Your Name <yangfulin123@qq.com>
2019-10-21 14:07:25 +0800

    question二级评论

--- src/main/java/com/community/exception/CustomizeErrorCode.java
+++ src/main/java/com/community/exception/CustomizeErrorCode.java
@@ -2,7 +2,7 @@ package com.community.exception;
 
 public enum  CustomizeErrorCode implements ICustomizeErrorCode{
 
-    QUESTION_NOT_FOUND(2001,"\"你找的问题被小僻菇吃掉了\""),
+    QUESTION_NOT_FOUND(2001,"\"你找的问题被吃掉了\""),
     TARGET_PARAM_NOT_FOUND(2002,"\"未选中任何问题或评论进行回复\""),
     NO_LOGIN(2003,"\"请登录后再进行当前操作\""),
     TYPE_PARAM_WRONG(2005,"\"评论类型错误或不存在\""),
