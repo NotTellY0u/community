72bfd9c6c7fb1d7da0a1c16b367721a0e604954d
Your Name <yangfulin123@qq.com>
2019-10-30 14:14:39 +0800

    相关问题

--- src/main/java/com/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/community/mapper/QuestionExtMapper.java
@@ -2,7 +2,12 @@ package com.community.mapper;
 
 import com.community.model.Question;
 
+import java.util.List;
+
 public interface QuestionExtMapper {
     int incView(Question record);
+
     int inCommentCount(Question record);
+
+    List<Question> selectRelated(Question question);
 }
